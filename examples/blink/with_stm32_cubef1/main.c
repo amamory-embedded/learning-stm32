ea364a7fa6e41e22f943d7165c7f5604324d9cba
Alexandre Amory <amamory@gmail.com>
2020-02-24 16:45:34 -0300

    blink with stm32_cubef1 is working with HAL

--- examples/blink/with_stm32_cubef1/main.c
+++ examples/blink/with_stm32_cubef1/main.c
@@ -1,29 +1,196 @@
-#include "stm32f1xx.h"
+/**
+  ******************************************************************************
+  * @file    Templates/Src/main.c
+  * @author  MCD Application Team
+  * @version V1.5.0
+  * @date    14-April-2017
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
 
-// Quick and dirty delay
-void delay(){
-    for (int i=0; i<1000000; i++) {
-        __asm__("nop");
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_rcc.h"
+
+#define LED1_PIN                         GPIO_PIN_13             /* PC.13*/
+#define LED1_GPIO_PORT                   GPIOC
+#define LED1_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOC_CLK_ENABLE()
+#define LED1_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOC_CLK_DISABLE()
+
+/** @addtogroup STM32F1xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStruct;
+
+    /* STM32F103xB HAL library initialization:
+      - Configure the Flash prefetch
+      - Systick timer is configured by default as source of time base, but user
+      can eventually implement his proper time base source (a general purpose
+      timer for example or other time source), keeping in mind that Time base
+      duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
+      handled in milliseconds basis.
+      - Set NVIC Group Priority to 4
+      - Low Level Initialization
+    */
+    HAL_Init();
+    SystemInit();
+    //SystemCoreClockUpdate();
+    /* Configure the system clock to 64 MHz */
+    SystemClock_Config();
+
+    /* Add your application code here
+    */
+
+    /* -1- Enable GPIO Clock (to be able to program the configuration registers) */
+    LED1_GPIO_CLK_ENABLE();
+
+    /* -2- Configure IO in output push-pull mode to drive external LEDs */
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLDOWN;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    GPIO_InitStruct.Pin = LED1_PIN;
+    HAL_GPIO_Init(LED1_GPIO_PORT, &GPIO_InitStruct);
+
+
+
+    /* Infinite loop */
+    while (1)
+    {
+        HAL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
+        /* Insert delay 100 ms */
+        HAL_Delay(100);
+
     }
 }
 
-int main (void) {
-    // Turn on the GPIOC peripheral
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-    // Put pin 13 in general purpose push-pull mode
-    GPIOC->CRH &= ~(GPIO_CRH_CNF13);
-    // Set the output mode to max. 2MHz
-    GPIOC->CRH |= GPIO_CRH_MODE13_1;
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 64000000
+  *            HCLK(Hz)                       = 64000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            PLLMUL                         = 16
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
 
-    while (1) {
-        // Reset the state of pin 13 to output low
-        GPIOC->BSRR = GPIO_BSRR_BR13;
-        delay();
-        // Set the state of pin 13 to output high
-        GPIOC->BSRR = GPIO_BSRR_BS13;
-        delay();
-    }
+  /* Configure PLL ------------------------------------------------------*/
+  /* PLL configuration: PLLCLK = (HSI / 2) * PLLMUL = (8 / 2) * 16 = 64 MHz */
+  /* PREDIV1 configuration: PREDIV1CLK = PLLCLK / HSEPredivValue = 64 / 1 = 64 MHz */
+  /* Enable HSI and activate PLL with HSi_DIV2 as source */
+  oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSI;
+  oscinitstruct.HSEState        = RCC_HSE_OFF;
+  oscinitstruct.LSEState        = RCC_LSE_OFF;
+  oscinitstruct.HSIState        = RCC_HSI_ON;
+  oscinitstruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  oscinitstruct.HSEPredivValue    = RCC_HSE_PREDIV_DIV1;
+  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSI_DIV2;
+  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
 
-    // Return 0 to satisfy compiler
-    return 0;
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1);
+  }
+
 }
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
